1db917bdcc168222ec22f58bb3acc26699479b48
Karim Abd El-Aziz <53383608+karimsoft@users.noreply.github.com>
2021-01-08 04:55:31 +0200

    Add files via upload

--- data/process_data.py
+++ data/process_data.py
@@ -60,9 +60,8 @@ def clean_data(df):
         # convert column from string to numeric
         categories[column] = categories[column].astype(int)
 		
-	# convert no. up one to one
-	if categories[column] >1:
-		categories[column] =1
+		# convert no. two to one
+		categories[column] =categories[column].replace("2","1")
 
     # drop the original categories column from `df`
     df.drop('categories', axis=1, inplace=True)
