b87209e2ab0c19c5e361451b9a6f5e049c58967c
Karim Abd El-Aziz <53383608+karimsoft@users.noreply.github.com>
2021-01-09 00:07:24 +0200

    Update run.py

--- app/run.py
+++ app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/MessageCategoriesETL.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('MessageCategoriesETL', engine)
 
 # load model
-model = joblib.load("../models/model.p")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
